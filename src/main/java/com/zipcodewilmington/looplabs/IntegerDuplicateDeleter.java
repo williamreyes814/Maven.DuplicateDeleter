fca970bb381b795ec9dafb0d581ed619934b5358
William Reyes <williamreyes814@yahoo.com>
2019-07-06 19:35:23 -0400

    finish

--- src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/IntegerDuplicateDeleter.java
@@ -1,5 +1,9 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by leon on 1/29/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +15,50 @@ public final class IntegerDuplicateDeleter extends DuplicateDeleter<Integer> {
 
     @Override
     public Integer[] removeDuplicates(int maxNumberOfDuplications) {
-        return new Integer[0];
+        List<Integer> list = new ArrayList<>();
+        for (int currentIndex = 0; currentIndex < array.length; currentIndex++) {
+            Integer currentValue = array[currentIndex];
+            Integer numberOfOccurrences = getNumberOfOccurrences(currentValue);
+            if(numberOfOccurrences < maxNumberOfDuplications) { // it has occurred at least `maxNumberOfTimes`
+                // add that element from the list
+                list.add(currentValue);
+            }
+        }
+
+        // convert to list to array
+        int sizeOfNewArray = list.size();
+        Integer[] newArrayToBePopulated = new Integer[sizeOfNewArray];
+        Integer[] array = list.toArray(newArrayToBePopulated);
+        return array;
     }
 
     @Override
     public Integer[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new Integer[0];
+        List<Integer> list = new ArrayList<>();
+        for (int currentIndex = 0; currentIndex < array.length; currentIndex++) {
+            Integer currentValue = array[currentIndex];
+            Integer numberOfOccurrences = getNumberOfOccurrences(currentValue);
+            if(numberOfOccurrences!= exactNumberOfDuplications) {
+                // remove that element from the list
+                list.add(currentValue);
+            }
+        }
+
+        // convert to list to array
+        int sizeOfNewArray = list.size();
+        Integer[] newArrayToBePopulated = new Integer[sizeOfNewArray];
+        Integer[] array = list.toArray(newArrayToBePopulated);
+        return array;
+    }
+
+    public Integer getNumberOfOccurrences(Integer value) {
+        Integer numberOfOccurrences = 0;
+        for (int currentIndex = 0; currentIndex < this.array.length; currentIndex++) {
+            Integer currentValue = array[currentIndex];
+            if(value == currentValue) {
+                numberOfOccurrences++;
+            }
+        }
+        return numberOfOccurrences;
     }
 }
--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,8 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +14,50 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        List<String> list = new ArrayList<>();
+        for (int currentIndex = 0; currentIndex < array.length; currentIndex++) {
+            String currentValue = array[currentIndex];
+            Integer numberOfOccurrences = getNumberOfOccurrences(currentValue);
+            if(numberOfOccurrences < maxNumberOfDuplications) { // it has occurred at least `maxNumberOfTimes`
+                // add that element from the list
+                list.add(currentValue);
+            }
+        }
+
+        // convert to list to array
+        int sizeOfNewArray = list.size();
+        String[] newArrayToBePopulated = new String[sizeOfNewArray];
+        String[] array = list.toArray(newArrayToBePopulated);
+        return array;
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        List<String> list = new ArrayList<>();
+        for (int currentIndex = 0; currentIndex < array.length; currentIndex++) {
+            String currentValue = array[currentIndex];
+            Integer numberOfOccurrences = getNumberOfOccurrences(currentValue);
+            if(numberOfOccurrences!= exactNumberOfDuplications) {
+                // remove that element from the list
+                list.add(currentValue);
+            }
+        }
+
+        // convert to list to array
+        int sizeOfNewArray = list.size();
+        String[] newArrayToBePopulated = new String[sizeOfNewArray];
+        String[] array = list.toArray(newArrayToBePopulated);
+        return array;
+    }
+
+    public Integer getNumberOfOccurrences(String value) {
+        Integer numberOfOccurrences = 0;
+        for (int currentIndex = 0; currentIndex < this.array.length; currentIndex++) {
+            String currentValue = array[currentIndex];
+            if(value == currentValue) {
+                numberOfOccurrences++;
+            }
+        }
+        return numberOfOccurrences;
     }
 }
